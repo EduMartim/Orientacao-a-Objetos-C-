ba6d23e129553d61f5c03075c8dac9921bdaecb3
Eduardo Martim <65382981+EduMartim@users.noreply.github.com>
2022-02-18 15:37:43 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Jogo_de_RPG_usando_OO.src.Entities;
+
+namespace Jogo_de_RPG_usando_OO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Jennica", 23, "White Wizard");
+
+            Console.WriteLine(wizard.Attack(1));
+            Console.WriteLine(wizard.Attack(7));
+
+
+        }
+    }
+}
